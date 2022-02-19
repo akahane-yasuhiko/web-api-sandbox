caf6df535d943f3206713e8fb401ddea01b3b4f1
cbh68880 <cbh68880@gmail.com>
2022-02-19 19:50:49 +0900

    new create

---
+++ DB_DEFINITIONN/uscm.mst_menu.sql
@@ -0,0 +1,5 @@
+insert into uscm.mst_menu(menu_id,menu_name) values 
+    ('m0001','ユーザー管理')
+  , ('m0002','ユーザー参照')
+  , ('m0003','メニュー管理')
+  , ('m0004','メニュー参照');
---
+++ DB_DEFINITIONN/uscm.mst_role.sql
@@ -0,0 +1,5 @@
+insert into uscm.mst_role(role_id,role_name) values 
+    ('r0001','m1adm')
+  , ('r0002','m1usr')
+  , ('r0003','m2adm')
+  , ('r0004','m2usr');
