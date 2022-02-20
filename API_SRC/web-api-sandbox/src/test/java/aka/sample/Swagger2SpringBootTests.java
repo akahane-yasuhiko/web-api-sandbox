8cfb4def2dcb85ef89053d9abdec3c84109c239b
cbh68880 <cbh68880@gmail.com>
2022-02-21 01:59:54 +0900

    不要なインポートを削除

--- API_SRC/web-api-sandbox/src/test/java/aka/sample/Swagger2SpringBootTests.java
+++ API_SRC/web-api-sandbox/src/test/java/aka/sample/Swagger2SpringBootTests.java
@@ -1,11 +1,9 @@
 package aka.sample;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration
 class Swagger2SpringBootTests {
 
 	@Test
